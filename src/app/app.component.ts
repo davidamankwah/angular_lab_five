1f779ebade5af2fbc2205d2eb24de33be0443331
davidamankwah <G00394825@gmit.ie>
2022-02-21 19:12:16 +0000

    lab upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'data-binding-app';
+  numberOfTimes = 0;
+  hidden =false;
+
+  count()
+  {
+    this.numberOfTimes++;
+  }
+
+  show()
+  {
+    if(this.hidden == true){
+      this.hidden = false;
+    }
+    else{
+      this.hidden = true;
+    }
+    
+  }
+
+
+}
